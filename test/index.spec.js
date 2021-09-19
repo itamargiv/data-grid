def98ac3628833e121210ca914f94df727c3eea0
Itamar Givon <itamar.givon.dev@gmail.com>
2021-09-19 14:43:09 +0200

    Add sorting

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,7 +7,9 @@ describe('DataGrid', () => {
         expect(grid instanceof DataGrid).toBe(true);
     });
 
-    it('instantiates from an array of key value tuple arrays', () => {
+    test.todo('instantiates from an array of arrays');
+
+    it('instantiates from an array of tuple arrays', () => {
         const fakeData = [[
             ['phrase', 'Hello World'],
             ['number', 42],
@@ -50,6 +52,10 @@ describe('DataGrid', () => {
         expect(grid.data).toEqual(expectedData);
     });
 
+    test.todo('instantiates from a tuple array of tuple arrays');
+    test.todo('instantiates from an object literal of object literals');
+    test.todo('instantiates from a Map of Map instances');
+    
     it('retrieves row as object literal', () => {
         const fakeData = [[
             ['phrase', 'Hello World'],
@@ -105,7 +111,174 @@ describe('DataGrid', () => {
         expect(grid.get(1, 'phrase')).toEqual('Goodbye Mars');
     });
     
-    test.todo('sorts data by column ascending');
-    test.todo('sorts data by column descending');
+    it('sorts data', () => {
+        const fakeData = [
+            [ 
+                ['phrase', 'Hello World'],
+                ['number', 42], 
+            ],
+            [ 
+                ['phrase', 'Goodbye Mars'],
+                ['number', 1986],
+            ],
+            [ 
+                ['phrase', 'So Long Venus'],
+                ['number', 1984],
+            ],
+            [ 
+                ['phrase', 'Bonjour Mercury'],
+                ['number', 35],
+            ],
+        ];
+
+        const grid = new DataGrid(fakeData);
+        const callback = (a, b) => a.phrase.localeCompare(b.phrase);
+
+        expect(grid.sort()).toEqual(grid.data.sort(callback));
+    });
+
+    it('sorts data descending', () => {
+        const fakeData = [
+            [ 
+                ['phrase', 'Hello World'],
+                ['number', 42], 
+            ],
+            [ 
+                ['phrase', 'Goodbye Mars'],
+                ['number', 1986],
+            ],
+            [ 
+                ['phrase', 'So Long Venus'],
+                ['number', 1984],
+            ],
+            [ 
+                ['phrase', 'Bonjour Mercury'],
+                ['number', 35],
+            ],
+        ];
+
+        const grid = new DataGrid(fakeData);
+        const callback = (a, b) => -a.phrase.localeCompare(b.phrase);
+
+        expect(grid.sort('desc')).toEqual(grid.data.sort(callback));
+    });
+
+    it('sorts data with callback', () => {
+        const fakeData = [
+            [ 
+                ['phrase', 'Hello World'],
+                ['number', 42], 
+            ],
+            [ 
+                ['phrase', 'Goodbye Mars'],
+                ['number', 1986],
+            ],
+            [ 
+                ['phrase', 'So Long Venus'],
+                ['number', 1984],
+            ],
+            [ 
+                ['phrase', 'Bonjour Mercury'],
+                ['number', 35],
+            ],
+        ];
+
+        const grid = new DataGrid(fakeData);
+        const callback = (a, b) => a.number - b.number;
+
+        expect(grid.sortWith(callback)).toEqual(grid.data.sort(callback));
+    });
+
+    it('sorts data by column', () => {
+        const fakeData = [
+            [ 
+                ['phrase', 'Hello World'],
+                ['number', 42],
+                ['isBoopy', true]
+            ],
+            [ 
+                ['phrase', 'Goodbye Mars'],
+                ['number', 1986],
+                ['isBoopy', false]
+            ],
+            [ 
+                ['phrase', 'So Long Venus'],
+                ['number', 1984],
+                ['isBoopy', false]
+            ],
+            [ 
+                ['phrase', 'Bonjour Mercury'],
+                ['number', 35],
+                ['isBoopy', true]
+            ],
+        ];
+
+        const grid = new DataGrid(fakeData);
+        const callback = (a, b) => a.number - b.number;
+
+        expect(grid.sortBy('number')).toEqual(grid.data.sort(callback));
+    });
+
+    it('sorts data by column descending', () => {
+        const fakeData = [
+            [ 
+                ['phrase', 'Hello World'],
+                ['number', 42],
+                ['isBoopy', true]
+            ],
+            [ 
+                ['phrase', 'Goodbye Mars'],
+                ['number', 1986],
+                ['isBoopy', false]
+            ],
+            [ 
+                ['phrase', 'So Long Venus'],
+                ['number', 1984],
+                ['isBoopy', false]
+            ],
+            [ 
+                ['phrase', 'Bonjour Mercury'],
+                ['number', 35],
+                ['isBoopy', true]
+            ],
+        ];
+
+        const grid = new DataGrid(fakeData);
+        const callback = (a, b) => -(a.number - b.number);
+
+        expect(grid.sortBy('number', 'desc')).toEqual(grid.data.sort(callback));
+    });
+
+    it('sorts data by column with callback', () => {
+        const fakeData = [
+            [ 
+                ['phrase', 'Hello World'],
+                ['number', 42],
+                ['isBoopy', true]
+            ],
+            [ 
+                ['phrase', 'Goodbye Mars'],
+                ['number', 1986],
+                ['isBoopy', false]
+            ],
+            [ 
+                ['phrase', 'So Long Venus'],
+                ['number', 1984],
+                ['isBoopy', false]
+            ],
+            [ 
+                ['phrase', 'Bonjour Mercury'],
+                ['number', 35],
+                ['isBoopy', true]
+            ],
+        ];
+
+        const grid = new DataGrid(fakeData);
+        const callback = (a, b) => -(a.isBoopy - b.isBoopy);
+
+        expect(grid.sortByWith('isBoopy', (a, b) => -(a-b)))
+            .toEqual(grid.data.sort(callback));
+    });
+
     test.todo('retrieves filtered data by callback');
 });
