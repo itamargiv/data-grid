e609793cdfd46bc9a63bb72e0ca9c14d72a778aa
Itamar Givon <itamar.givon.dev@gmail.com>
2021-10-06 22:19:37 +0300

    Add type annotations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
+// TODO: Add TS in JSdoc
+// TODO: Re-examine instantiation api. For MVP only support array of arrays, array of objects, object of objects.
+
+// @ts-check
+
+import { ORDER } from './types';
+
+/**
+ * @typedef { import('./types').Index } Index
+ * @typedef { import('./types').Keyed<any> } KeyedValue
+ * @typedef { import('./types').Order } Order
+ * @typedef { import('./types').Row } Row
+ */
+
 class DataRow {
+    /**
+     * @type {Index[]}
+     */
     #columns = null;
+
+    /**
+     * @type {ReadonlyMap<Index, any>}
+     */
     #map = null;
 
+    /**
+     * DataRow represents a row of data in a data grid.
+     * Each piece of data is keyed by it's respective column header.
+     *
+     * @param {any[]} data An array of the row's data
+     * @param {Index[]} columns An array of column headers
+     */
     constructor(data, columns = []) {
         this.#columns = columns;
+
+        /**
+         * @type {Iterable<KeyedValue>}
+         */
         const pairs = data.map((value, i) => [
             this.#columns[i] ? this.#columns[i] : i,
             value,
@@ -12,6 +44,11 @@ class DataRow {
         this.#map = new Map(pairs);
     }
 
+    /**
+     * Retrieve the data from the row as a serialized object.
+     *
+     * @returns {Row}
+     */
     get cells() {
         return Array.from(this.#map).reduce(
             (literal, [key, val]) => ({
@@ -22,17 +59,48 @@ class DataRow {
         );
     }
 
+    /**
+     * Retrieve data from a row by it's key.
+     *
+     * @param {Index} c Column name or number to retrieve info from
+     * @returns {any}
+     */
     get(c) {
         return this.#map.get(c);
     }
 }
 
 class DataGrid {
+    /**
+     * @type {DataRow[]}
+     */
     #grid = null;
+
+    /**
+     * @type {Index[]}
+     */
     #columns = [];
+
+    /**
+     * @type {Intl.Collator}
+     */
     #collator = null;
+
+    /**
+     * @type {string}
+     */
     #locale;
 
+    /**
+     * DataGrid represent an immutable grid of values which can by operated on
+     * in the same fashion as arrays. i.e. a grid that can be sorted, filtered,
+     * mapped and reduced.
+     *
+     * @param {any[][]} data A two dimensional array containing values per row
+     * @param {Index[]} columns An array of column headers
+     * @param {string} locale A string representing a language locale, such as
+     *                        `he`, `fr` or `en-GB`
+     */
     constructor(data = [], columns, locale = 'en') {
         this.#grid = data.map((row) => new DataRow(row, columns));
         this.#columns = columns;
@@ -42,14 +110,23 @@ class DataGrid {
         });
     }
 
+    // TODO: Find a way to represent internal data of grid in a similar fashion  to valueOf
     /**
-     * TODO: Find a way to represent internal data of grid in a similar fashion
-     *       to valueOf
+     * @returns {Row[]}
      */
     get data() {
         return this.#grid.map((row) => row.cells);
     }
 
+    /**
+     * Creates a default sorting function for a given column. The default
+     * sorting function will order by numeric value first, and the will compare
+     * unicode values according to the provided locale.
+     *
+     * @param {Index} column The column name to sort
+     * @param {Order} order The order in which to sort by
+     * @returns {(a: any, b: any) => number}
+     */
     #defaultSortFor(column, order) {
         return (a, b) => {
             const ordering = this.#collator.compare(a[column], b[column]);
@@ -57,35 +134,99 @@ class DataGrid {
         };
     }
 
+    /**
+     * Retrieves an object serialized row from the grid.
+     *
+     * @param {number} r A row index in the grid to retrieve the row from
+     * @returns {Row}
+     */
     row(r) {
         return this.#grid[r].cells;
     }
 
+    /**
+     * Retrieves a column of values from the grid, by the column name or index.
+     *
+     * @param {Index} c The column to retrieve values for
+     * @returns any[]
+     */
     col(c) {
         return this.#grid.map((row) => row.get(c));
     }
 
+    /**
+     * Retrieves data from a particular cell in the grid.
+     *
+     * @param {number} r A row index in the grid to retrieve the row from
+     * @param {Index} c An column index to retrieve from the specified row
+     * @returns {any}
+     */
     get(r, c) {
         return this.#grid[r].get(c);
     }
 
-    sort(order = 'asc') {
-        return this.data.sort(this.#defaultSortFor(this.#columns[0], order));
+    /**
+     * Returns the data of the grid, sorted by a given order
+     * for the first column.
+     *
+     * @param {Order} order The order in which to sort by
+     * @returns {Row[]}
+     */
+    sort(order = ORDER.ASC) {
+        return this.sortWith(this.#defaultSortFor(this.#columns[0], order));
     }
 
+    /**
+     * Returns the data of the grid, as sorted by a given sorting function.
+     *
+     * @param {(a: any, b: any) => number} cb A callback to determine the sort
+     *                                        order of two values in the grid
+     * @returns
+     */
     sortWith(cb) {
         return this.data.sort(cb);
     }
 
+    /**
+     * Returns the data of the grid, as sorted by a given column with the
+     * default sorting function.
+     *
+     * @param {Index} column A column to sort the data by
+     * @param {Order} order The order in which to sort by
+     * @returns {Row[]}
+     */
     sortBy(column, order) {
         return this.sortWith(this.#defaultSortFor(column, order));
     }
 
+    /**
+     * Returns the data of the grid, as sorted by a given column with the
+     * provided sorting function.
+     *
+     * @param {Index} column A column to sort the data by
+     * @param {(a: any, b: any) => number} cb A callback to determine the sort
+     *                                        order of two values in the grid
+     * @returns {Row[]}
+     */
     sortByWith(column, cb) {
         return this.sortWith((a, b) => cb(a[column], b[column]));
     }
 
+    /**
+     * Instantiates a data grid from a two dimensional array of key value pairs
+     *
+     * @param {KeyedValue[][]} data A two dimensional array of arrays of key
+     *                              value pairs
+     * @returns DataGrid
+     */
     static fromPairs(data) {
+        /**
+         * Reduces key value pairs into DataGrid constructor arguments
+         *
+         * @param {[any[][], Index[]]} acc A pair of
+         * @param {KeyedValue[]} row
+         * @returns {[any[][], Index[]]}
+         */
         const pairReducer = (acc, row) => {
             const headers = row.map((pair) => pair[0]);
             const values = row.map((pair) => pair[1]);
@@ -100,12 +241,25 @@ class DataGrid {
         return new DataGrid(...args);
     }
 
+    /**
+     * Instantiates a data grid from an array of object literals
+     *
+     * @param {Object[]} data An array of objects where the object keys
+     *                        represent the grid columns
+     * @returns DataGrid
+     */
     static fromObjects(data) {
         return DataGrid.fromPairs(
             data.map((literal) => Object.entries(literal))
         );
     }
 
+    /**
+     * Instantiates from an array of ES6 Maps.
+     *
+     * @param {ReadonlyMap<Index, any>[]} data An array of maps
+     * @returns DataGrid
+     */
     static fromMaps(data) {
         return DataGrid.fromPairs(
             data.map((rowMap) => Array.from(rowMap.entries()))
---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export enum Order {
+    ASC = 'asc',
+    DESC = 'desc',
+}
+
+/** @enum {Order} */
+export const ORDER = {
+    ASC: Order.ASC,
+    DESC: Order.DESC,
+};
+
+export type Index = string | number;
+
+type Pair<a, b> = [a, b];
+export type Keyed<T> = Pair<Index, T>;
+
+export interface Row {
+    [key: Index]: any;
+}
